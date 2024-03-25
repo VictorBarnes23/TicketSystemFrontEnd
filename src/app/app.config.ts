e08ad8996103a3e10001a174c0c673bc3e5d0892
Victor <vicpbarnes17@gmail.com>
2024-03-24 22:20:02 -0400

    Set up basic google login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,37 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import {
+  SocialLoginModule,
+  SocialAuthServiceConfig,
+  GoogleLoginProvider,
+} from "@abacritt/angularx-social-login";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    //We are building a provider. Version 2.2.0 of package doesn't provide one.
+    {
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            //Remove the .apps.googleusercontent.com from the client id
+            //MAKE SURE TO HIDE IT FROM GITHUB
+            "604360389729-bsrd8673obr8bjsocvt4nulrspv447mc"
+          ),
+        },
+      ],
+      onError: (err) => {
+        debugger;
+        console.error(err);
+      },
+    } as SocialAuthServiceConfig,
+  },
+  //more providers can go here
+]
 };
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,31 @@
+import { GoogleSigninButtonModule, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,GoogleSigninButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  user: SocialUser = {} as SocialUser;
+  loggedIn: boolean = false;
 
+
+  constructor(private socialAuthServiceConfig: SocialAuthService) { }
+ 
+  ngOnInit() {
+    //authState is a custom observable that will run again any time changes are noticed.
+    this.socialAuthServiceConfig.authState.subscribe((userResponse: SocialUser) => {
+      this.user = userResponse;
+      //if login fails, it will return null.
+      this.loggedIn = (userResponse != null);
+    });
+  }
+  //login component doesn't account for logging out.
+  signOut(): void {
+    this.socialAuthServiceConfig.signOut();
+  }
 }
